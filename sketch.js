11eee97c2c5f8f955693d9523c0511e74b955a0c
HartejAnand <76600578+HartejAnand@users.noreply.github.com>
2021-03-26 15:10:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -61,6 +61,6 @@ function draw() {
 
 keyPressed=function(){
 	if(keyDown("left")){
-		Matter.Body.applyForce(ball1.body,ball1.body.position,{x:-1000,y:-1000});
+		Matter.Body.applyForce(ball1.body,ball1.body.position,{x:-100,y:-100});
 	}
 };
